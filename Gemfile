32268240702b9fcf0795475ce01b78007119ca0d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-03 19:16:42 +0300

    add message tablle

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+# to use beautiful console
+gem 'hirb'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_154720) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_161057) do
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
