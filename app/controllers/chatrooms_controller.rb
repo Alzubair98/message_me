2c14676848d0fd17f4b672f672048524f99d3cb0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-04 22:17:30 +0300

    allow users to send messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,16 @@
 class ChatroomsController < ApplicationController 
+    before_action :require_user
+
     def index 
+        @message = Message.new
         @messages = Message.all
+    end
+
+    def new 
+
+    end
+
+    def create 
+
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController 
+    before_action :logged_in_redirect, only: [:new, :create]
+    
     def new 
 
     end
@@ -19,5 +21,14 @@ class SessionsController < ApplicationController
         reset_session
         flash[:success] = "You have successfully logged out"
         redirect_to login_path
+    end
+
+    private 
+
+    def logged_in_redirect 
+        if logged_in?
+            flash[:error] = "You are already logged in"
+            redirect_to root_path
+        end
     end
 end
