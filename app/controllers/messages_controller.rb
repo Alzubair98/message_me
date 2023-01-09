f5a7d6cf84b835d48b43fde46cb43bb570f80642
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-10 00:01:04 +0300

    add delete function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,32 @@
 class MessagesController < ApplicationController 
     before_action :require_user
+    before_action :set_message, only: [:destroy]
+    before_action :require_same_user, only: [:destroy]
 
-
-    def create 
+    def create
         message = current_user.messages.build(message_params)
         if message.save 
             ActionCable.server.broadcast "chatroom_channel", { body: message_render(message) }
         end
     end
 
-    
+    def destroy
+        @message.destroy
+        redirect_to root_path
+    end
 
     private
+
+    def set_message 
+        @message = Message.find(params[:id])
+    end
+
+    def require_same_user 
+        if current_user != @message.user
+            flash[:error] = "You can only delete your own messages"
+            redirect_to root_path
+        end
+    end
 
     def message_params 
         params.require(:message).permit(:body)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :messages, except: [:create, :new]
   post 'message', to: 'messages#create'
 
   mount ActionCable.server, at: '/cable'
