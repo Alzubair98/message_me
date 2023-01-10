71661ac246ed0c90a71ecf5c09bc6951c305656a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-10 11:04:58 +0300

    add devise configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # devise_scope :user do 
+  #   get 'users', to: 'devise/sessions#new'
+  # end
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   root 'chatrooms#index'
